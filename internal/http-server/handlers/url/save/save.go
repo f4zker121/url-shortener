c9096f99cf64573640a3d2645d5fcffd226793e2
f4zker121 <utkonos322@gmail.com>
2024-05-18 23:19:44 +0300

    .

---
+++ internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,97 @@
+package save
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	resp "url/internal/lib/api/response"
+	"url/internal/lib/logger/sl"
+	"url/internal/lib/random"
+	"url/internal/storage"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+)
+
+type Request struct {
+	URL   string `json:"url" validate:"required,url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	resp.Response
+	Alias string `json:"alias,omitempty"`
+}
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) (int64, error)
+}
+
+const aliasLenght = 6
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.ValidationsError(validateErr))
+
+			return
+		}
+
+		alias := req.Alias
+
+		if alias == "" {
+			alias = random.NewRandomString(aliasLenght)
+		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("url already exists", slog.String("url", req.URL))
+
+			render.JSON(w, r, resp.Error("url already exists"))
+
+			return
+		}
+
+		if err != nil {
+			log.Error("failed to add url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to add url"))
+
+			return
+		}
+
+		log.Info("url added", slog.Int64("id", id))
+
+		render.JSON(w, r, Response{
+			Response: resp.OK(),
+			Alias:    alias,
+		})
+	}
+}
